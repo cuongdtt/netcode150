c1d0ae991b9b488915263774c80c33cf44ef0fb7
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2025-07-06 21:02:02 +0700

    1231 best time to buy and sell stock

--- SlidingWindow/121.best-time-to-buy-and-sell-stock.js
+++ SlidingWindow/121.best-time-to-buy-and-sell-stock.js
@@ -2,6 +2,29 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {};
+// O(n^2) failure performance
+// var maxProfit = function (prices) {
+//   let max = 0;
+//   for (let i = 0; i < prices.length; i++) {
+//     for (let j = i; j < prices.length; j++) {
+//       if (prices[j] - prices[i] > max) {
+//         max = prices[j] - prices[i];
+//       }
+//     }
+//   }
+//   return max;
+// };
+
+var maxProfit = function (prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i]);
+    let currentProfit = prices[i] - minPrice;
+
+    maxProfit = Math.max(maxProfit, currentProfit);
+  }
+  return maxProfit;
+};
 
 module.exports = { maxProfit };
